0ba64682759ddcea833c3dbb4ba6897b0a69110e
r3pmah <r3pmah@gmail.com>
2024-10-12 10:46:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        System.out.println(numbers[0]);
+        System.out.println(numbers[1]);
+        String[] strings = new String[3];
+          strings[0] = "hello";
+        strings[1] = "good morning";
+        strings[2] = "good night";
+        System.out.println(strings[0]);
+
+
+
+    }
+}
